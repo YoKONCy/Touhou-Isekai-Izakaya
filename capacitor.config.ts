8af6ed07cd4d170beac50da8a28ee10fc7397628
YoKONCy <2752026184@qq.com>
2026-02-02 04:49:47 +0800

    feat: rename app to '东方异界食堂' and update app icon with 本居小铃

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.touhou.isekai.izakaya',
-  appName: 'Touhou Isekai Izakaya',
+  appName: '东方异界食堂',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
